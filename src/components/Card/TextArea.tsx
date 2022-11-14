cc77d54402bff830b5be0bba93ad85dec18c9bd5
Grégori Sabel <seriosabel57@gmail.com>
2022-11-14 18:19:19 -0300

    text area auto-growth

--- src/components/Card/TextArea.tsx
+++ src/components/Card/TextArea.tsx
@@ -1,47 +1,47 @@
-import { useEffect, useState } from "react";
-import { Button } from "../Button";
-import { Text } from "../Text";
-import { BsInfoCircle } from 'react-icons/bs' 
-import { Tooltip } from "../Tooltip";
 
 
-interface CardProps{
-  id: number;
-  initialQuestionText: string;
-  initialAnswerText: string;
-}
-
-
-interface TextAreaProps{
+interface TextAreaProps extends React.HTMLProps<HTMLTextAreaElement>{
   isEditingCard: boolean;
   setCurrentText(innerText: string): void;
-  currentText: string
+  currentText: string;
+  className?: string;
 }
 
 
-export function TextArea({ isEditingCard, setCurrentText, currentText }: TextAreaProps){
+export function TextArea({ 
+  isEditingCard, 
+  setCurrentText, 
+  currentText, 
+  className,
+  ...rest
+}: TextAreaProps){
+
+  function adjustHeight(el: HTMLTextAreaElement){
+    el.style.height = '1px';
+    el.style.height = (el.scrollHeight)+"px";
+  }
 
 
   return (
-    <div 
-      role={'textbox'} 
-      contentEditable={isEditingCard}
-      onChange={(e: React.ChangeEvent<HTMLInputElement> ) => setCurrentText(e.target.innerText)}
+    <textarea readOnly={!isEditingCard}
+      onKeyUp={(e: any ) => adjustHeight(e.target)}
+      value={currentText}
+      onChange={(e) => setCurrentText(e.target.value)}
       className={`
+        overflow-hidden
         leading-relaxed
         w-full
         text-neutral-50
         bg-neutral-900
-        overflow-visible
         outline-none
         decoration-gray-900
         resize-none
         text-justify
         font-serif
-      `}  
-      >
-        {currentText}
-      </div>
+        ${className}
+      `} 
+      {...rest}
+    />
 
   )
 }
